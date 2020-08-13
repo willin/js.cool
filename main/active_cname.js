1ea78556fbe81dc7685ae9bc0004489e4050fdb3
dirkhe1051931999 <1051931999@qq.com>
2020-08-13 15:48:48 +0800

    add a cname

--- main/active_cname.js
+++ main/active_cname.js
@@ -32,6 +32,7 @@ module.exports = {
   'speech': 'willin.github.io',
   'antiadblock': 'js-cool.github.io',
   'swm': 'shiwangme.github.io',
+  'jen': 'dirkhe1051931999.github.io',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
