775e9e74709dc97d27f0dad91297f1edcbb52a69
kaodan <36099161+kaodan@users.noreply.github.com>
2021-08-10 16:03:23 +0800

    Update active_cname.js

--- main/active_cname.js
+++ main/active_cname.js
@@ -56,6 +56,7 @@ module.exports = {
   'wangdabao-resume': 'wangdabaoqq.github.io/resume-new',
   'wangdabao': 'wangdabaoqq.github.io/hexo',
   'zwtdwzym': 'zwtduserid.github.io',
+  'kaodan': 'kaodan.github.io'
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
