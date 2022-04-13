2d992aad570c5d31e0c8ffda787eb570c10fe9ff
zeusgangzz <103348004+zeusgangzz@users.noreply.github.com>
2022-04-13 09:11:35 +0700

    Update active_cname.js (#84)

--- main/active_cname.js
+++ main/active_cname.js
@@ -87,6 +87,7 @@ module.exports = {
   'notapi': 'notapi.vercel.app', //noCF
   'heiguoblog': 'fairy-plus.github.io',
   'zenobia': 'zenobiadevelopment.github.io',
+  'ngcchinl': 'zeusgangzz.github.io',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
