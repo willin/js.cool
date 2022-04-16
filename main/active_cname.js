0d427d80c4935f72a4ecf86e11d295e2e0dd602b
pzwboy <88925003+pzwboy@users.noreply.github.com>
2022-04-16 10:14:33 +0800

    Update active_cname.js

--- main/active_cname.js
+++ main/active_cname.js
@@ -88,6 +88,7 @@ module.exports = {
   'heiguoblog': 'fairy-plus.github.io',
   'zenobia': 'zenobiadevelopment.github.io',
   'ngcchinl': 'zeusgangzz.github.io',
+  'pzwboy': 'pzwboy.github.io',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
