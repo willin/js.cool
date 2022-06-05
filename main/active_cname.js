b6f9c699d559e2b4a95104685e8228edeb663cda
pzwboy <88925003+pzwboy@users.noreply.github.com>
2022-06-05 16:53:10 +0800

    Update active_cname.js

--- main/active_cname.js
+++ main/active_cname.js
@@ -96,7 +96,6 @@ module.exports = {
   'heiguoblog': 'fairy-plus.github.io',
   'zenobia': 'zenobiadevelopment.github.io',
   'ngcchinl': 'zeusgangzz.github.io',
-  'pzwboy': 'pzwboy.github.io',
   'connor': 'connordoesdev.github.io',
   'mew': 'meowier.github.io',
   'ethan': 'ethan-phu.github.io',
