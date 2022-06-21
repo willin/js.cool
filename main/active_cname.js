735644de52470588b87854482740eee8bfbb2c76
plurryt <70047632+pythoniaweb@users.noreply.github.com>
2022-06-22 02:16:40 +0300

    world.js.cool (#101)
    * world.js.cool
    
    Created page.
    Added world.js.cool subdomain
    
    * Update active_cname.js
    
    Co-authored-by: Willin Wang <willin@willin.org>

--- main/active_cname.js
+++ main/active_cname.js
@@ -106,6 +106,7 @@ module.exports = {
   'eleven': 'elevenvac.github.io',
   'wsee': 'md-wz.vercel.app', //noCF
   'wlcf': 'md-wz.vercel.app', //noCF
+  'world': 'pythoniaweb.github.io',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
