c60b973660e59e766b53deebb4521dcec73bfed6
ayahchill <108823364+ayahchill@users.noreply.github.com>
2022-07-09 22:26:34 +0700

    Update active_cname.js

--- main/active_cname.js
+++ main/active_cname.js
@@ -110,6 +110,7 @@ module.exports = {
   'world': 'pythoniaweb.github.io',
   'frank': 'joyfrank.vercel.app',// noCF
   'class20': 'redblueidea.github.io',
+  'radio': 'ayahchill.github.io',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
