76b41fbb431afc1e55b4f52dfc7760dbdaa4cd4b
zxrpn <91787031+zxrpn@users.noreply.github.com>
2022-07-13 10:04:52 +0800

    Update active_cname.js (#111)

--- main/active_cname.js
+++ main/active_cname.js
@@ -113,6 +113,7 @@ module.exports = {
   'class20': 'redblueidea.github.io',
   'radio': 'ayahchill.github.io',
   'start': 'start-pages.vercel.app',//noCF
+  'zxr': 'zxrpn.github.io',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
