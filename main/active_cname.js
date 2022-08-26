3c03e89d4c8e6eddd48adda61d4d5f8fa162ae9e
AceyGoesBoom <83757443+AceyDoCodes@users.noreply.github.com>
2022-08-26 17:03:35 +0700

    using ns instead

--- main/active_cname.js
+++ main/active_cname.js
@@ -130,6 +130,5 @@ module.exports = {
   // 以下为已启用的保留域名，请勿修改
   '@': 'js-cool.github.io',
   'www': 'kv6xcc.coding-pages.com', // 国内镜像
-  'logo': 'js-cool.github.io',
-  'acey': 'cname.vercel-dns.com'
+  'logo': 'js-cool.github.io'
 };
