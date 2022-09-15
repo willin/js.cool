d2313037055f8f5ac013c6982a238eaec9c3f12f
Deng-Duck <93126855+Deng-Duck@users.noreply.github.com>
2022-09-15 16:44:04 +0800

    pull rep
    1

--- main/active_cname.js
+++ main/active_cname.js
@@ -126,6 +126,7 @@ module.exports = {
   'elvis': 'elvisndubuisi.vercel.app',//nocF
   'dinglin': 'roaring-churros-e65865.netlify.app',// noCF
   'thitit': 'portfolio-five-tan.vercel.app',
+  'duck': 'deng-duck.github.io',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
