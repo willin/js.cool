8a23d9e9e3347a90a1237fbb85a7c7b0c625b4df
XiaoyuZhao <49837632+ZXYFrank@users.noreply.github.com>
2022-11-21 11:01:12 +0800

    Update active_cname.js

--- main/active_cname.js
+++ main/active_cname.js
@@ -146,6 +146,7 @@ module.exports = {
   'dengduck':'dengduck.netlify.app',
   'waio': 'cname.vercel-dns.com', // noCF
   'joy': 'frankjoy.vercel.app',// noCF
+  'journey': 'feynmanfrank.github.io'// noCF
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
