e0ca10006ce7e0bfc353c49783dd6e65fc63764b
Zeptar <99463015+Zeptar1069@users.noreply.github.com>
2022-11-27 15:54:32 +1300

    Add subdomain: zeptar (#191)
    * Add subdomain: zeptar
    
    * Update active_cname.js
    
    Co-authored-by: Willin Wang <willin@willin.org>

--- main/active_cname.js
+++ main/active_cname.js
@@ -150,6 +150,7 @@ module.exports = {
   'joy': 'frankjoy.vercel.app',// noCF
   'journey': 'feynmanfrank.github.io',// noCF
   'dhruvil':'dhruvilmoradiya.github.io',
+  'zeptar': 'zeptar.vercel.app',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
