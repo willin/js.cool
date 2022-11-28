95603d188ba5b3e2f454798395ec10a86dc26c00
Dhruvil Moradiya <108447840+DhruvilMoradiya@users.noreply.github.com>
2022-11-28 22:37:29 +0530

    added new domain (#192)

--- main/active_cname.js
+++ main/active_cname.js
@@ -150,6 +150,7 @@ module.exports = {
   'joy': 'frankjoy.vercel.app',// noCF
   'journey': 'feynmanfrank.github.io',// noCF
   'dhruvil':'dhruvilmoradiya.github.io',
+  'whatsme.dhruvil':'whatsme.netlify.app'
   'zeptar': 'zeptar.vercel.app',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
