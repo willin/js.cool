ee97ed885067411e17d0282abecc96825cc2e72c
CKY <awa@outlook.ie>
2022-12-06 07:52:22 +0800

    Update active_cname.js

--- main/active_cname.js
+++ main/active_cname.js
@@ -154,6 +154,7 @@ module.exports = {
   'whatsme.dhruvil':'whatsme.netlify.app'
   'zeptar': 'zeptar.vercel.app',
   'muyangplus': 'muyangplus.github.io',
+  'eshexon': 'eshexon-docs.netlify.app', // noCF
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
