9a7c7951233006b8d4e97a24b123b5f1224b2499
Willin 王初瘦 <willin@willin.org>
2023-01-19 11:48:06 +0800

    Update active_cname.js

--- main/active_cname.js
+++ main/active_cname.js
@@ -162,7 +162,7 @@ module.exports = {
   'xlenco'：'cname.vercel-dns.com', //noCF
   'yxlr': 'regal-gnome-0c8689.netlify.app', //noCF
   'gtcblog'：'gtcblog.netlify.app', //noCF
-  'willmo.cms':'cname.vercel-dns.com.',
+  'willmo.cms':'cname.vercel-dns.com', //noCF
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
