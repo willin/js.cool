4b4a29559094d4e9a739fe8cc39ed9f672e09fcb
Willmo <72198229+Dr-WillMos@users.noreply.github.com>
2023-02-06 09:40:00 +0800

    Update and rename main/active_cname.js to main /active_cname.js

--- main /active_cname.js
+++ main /active_cname.js
@@ -144,7 +144,7 @@ module.exports = {
   'zzy':'hitptep.github.io',//noCF
   'abe':'hitptep.github.io',//noCF
   'dice': 'cname.vercel-dns.com', //noCF
-  'willmo': 'willmo.netlify.app', 
+  'willmo': 'dr-willmos.github.io', 
   'navigator':'navigator.github.io',
   'zswangziye':'zswangziye.github.io',
   'duckwaline':'waline-3bg5otthj-deng-duck.vercel.app',
