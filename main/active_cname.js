942188f7be72577655aab8f36de9cb1f09f385d2
wszmk <125188461+wszmk@users.noreply.github.com>
2023-02-25 18:33:18 +0800

    Update active_cname.js

--- main/active_cname.js
+++ main/active_cname.js
@@ -172,6 +172,7 @@ module.exports = {
   'lan':'LanHuang025.github.io',
   'geekswg':'geekswg.github.io',
   'geek-waline': 'geekswg.vercel.app', //noCF
+  'ze':'wszmk.github.io/blog',
 
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
