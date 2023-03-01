95dceccfbc5c1a85655e97f3279ebd5b486b6334
ansari-saify <saify.ansari@acquaintsoft.com>
2023-03-01 18:19:38 +0530

    Add cool domain

--- main/active_cname.js
+++ main/active_cname.js
@@ -171,6 +171,7 @@ module.exports = {
   'geekswg':'geekswg.github.io',
   'geek-waline': 'geekswg.vercel.app', //noCF
   'fun': 'michaelsynan.github.io',
+  'ansari-saify':'ansari-saify.github.io',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
