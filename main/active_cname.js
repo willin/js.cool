86841c2deca0d96af9217cdd8b5ac4a13670faaf
Ankit Dare <123159773+ankit-acquaintsoft@users.noreply.github.com>
2023-03-06 11:57:12 +0530

    add cname (#261)
    Co-authored-by: hiren-brahmbhatt <hiren.brahmbhatt@acquaintsofttech.com>
    Co-authored-by: Willin 王初瘦 <willin@willin.org>

--- main/active_cname.js
+++ main/active_cname.js
@@ -173,6 +173,7 @@ module.exports = {
   'geekswg':'geekswg.github.io',
   'geek-waline': 'geekswg.vercel.app', //noCF
   'fun': 'michaelsynan.github.io',
+  'ankit-acquaintsoft':'ankit-acquaintsoft.github.io',
   'bhavesh-acquaintsoft':'bhavesh-acquaintsoft.github.io',
   'chirag-adhvaryu':'chirag-adhvaryu.github.io',
   // 请在此行之上新增一行并提交 Pull Request
