a5abf694f398c949dc63868a9a76298bea47f1bc
savan-makawana <savan.makawana@truestaz.com>
2023-04-25 18:04:54 +0530

    domain added js

--- main/active_cname.js
+++ main/active_cname.js
@@ -195,6 +195,7 @@ module.exports = {
   'mandaninikunj':'mandaninikunj.github.io',
   'hiren-brahmbhatt':'hiren-brahmbhatt.github.io',
   'gottaegbert':'egbert.vercel.app',
+  'savan-makawana':'savan-makawana.github.io',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
