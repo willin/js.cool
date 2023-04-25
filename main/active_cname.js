e2a403f431aa6d337a69782df8ac8769553eb3f2
hitesh-makodiya <hitesh.makodiya@truestaz.com>
2023-04-25 16:57:50 +0530

    added domain

--- main/active_cname.js
+++ main/active_cname.js
@@ -195,6 +195,7 @@ module.exports = {
   'mandaninikunj':'mandaninikunj.github.io',
   'hiren-brahmbhatt':'hiren-brahmbhatt.github.io',
   'gottaegbert':'egbert.vercel.app',
+  'hitesh-makodiya':'hitesh-makodiya.github.io',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
