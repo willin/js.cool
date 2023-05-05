8170582acc20b440dc5a3a9d82f784583082a1cc
zcns <130817248+zcns@users.noreply.github.com>
2023-05-05 17:15:35 +0800

    Update active_cname.js

--- main/active_cname.js
+++ main/active_cname.js
@@ -195,7 +195,7 @@ module.exports = {
   'mandaninikunj':'mandaninikunj.github.io',
   'hiren-brahmbhatt':'hiren-brahmbhatt.github.io',
   'gottaegbert':'egbert.vercel.app',
-  'oa':'zcns.github.io',
+  'oa':'zcns.github.io/vmss/',
   // 请在此行之上新增一行并提交 Pull Request
   // 示例：
   // 'youarname': 'username.github.io' // noCF
