dce2a4b746283f7f22c65af41cce3c75eda4a450
ririko5834 <64729293+ririko5834@users.noreply.github.com>
2023-05-26 15:41:18 +0200

    Update active_ns.js

--- main/active_ns.js
+++ main/active_ns.js
@@ -24,7 +24,8 @@ module.exports = {
   yuuuics: ['ns1.vercel-dns.com', 'ns2.vercel-dns.com'],
   crypt: ['ns1.epizy.com', 'ns2.epizy.com'],
   mc: ['ns1.vercel-dns.com', 'ns2.vercel-dns.com'],
-  dast: ['adam.ns.cloudflare.com', 'marjory.ns.cloudflare.com']
+  dast: ['adam.ns.cloudflare.com', 'marjory.ns.cloudflare.com'],
+  datality: ["ns1.zilore.net", "ns2.zilore.net"]
 };
 
 /*
